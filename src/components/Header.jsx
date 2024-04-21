361df46ccee8ad8ed64cc87c9ae3b099e6d54ed7
tonnguyen <tonngokhong_75@yahoo.com>
2024-04-21 09:42:26 +0700

    fix code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
+import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
+import { useContext } from 'react';
+import CartContext from '../store/CartContext.jsx';
+import {UserProgressContext} from '../store/UserProgressContext.jsx';
 
 export default function Header() {
-    return  <header id="main-header" >
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+    
+    const totalCartItems = cartCtx.items.reduce((tottalNumberOfItems, item) => {
+        return tottalNumberOfItems + item.quantity;
+    }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
+
+    return(
+
+      <header id="main-header" >
         <div id="title">
             <img src={logoImg} alt="A restaurent" />
             <h1>ReactFood</h1>
         </div>
         <nav>
-            <button>Cart (0)</button>
+            <Button textOnly onClick={handleShowCart}>
+                Cart ({totalCartItems})</Button>
         </nav>
     </header>
+    );
 }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -14,7 +14,7 @@ export default function Meals() {
     }
     useEffect(() => {
  
-s
+
     fetchMeals();
     },[]);
 
